97ee2a36592fccf40c8239dd708b981143318571
Alex <aleksey33333@yandex.ru>
2021-06-04 10:46:21 +0400

    Changed db to properties.py file
    I think we don't need db in this project

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import torchvision.transforms as transforms
 from PIL import Image
 
 import style_transformer as st_tr
-import database
+import properties as prop
+import tokens
 
 content_img = Image.open("images/content_image.jpg")
 style_img = Image.open("images/style_image.jpg")
-
-prop = database.Properties()
 
 image_transformer = st_tr.ImageTransformer()
 content_img, style_img = image_transformer(content_img, style_img)
 
 style_transformer = st_tr.StyleTransformer()
-tensor_image = style_transformer(content_img, style_img, num_steps=prop.num_steps).squeeze(0)
+tensor_image = style_transformer(content_img, style_img, num_steps=prop.NUM_STEPS).squeeze(0)
 image = transforms.ToPILImage(mode='RGB')(tensor_image)
 image.save("images/output.jpg")
