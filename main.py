bc435d4fa5c88c837752d9c1802bb5bd96919314
Alex <aleksey33333@yandex.ru>
2021-06-04 12:28:29 +0400

    Added first funcitonality of telegram bot
    Functionality: /start, /help, echo messages and pictures sending

--- main.py
+++ main.py
@@ -7,6 +7,37 @@ import style_transformer as st_tr
 import properties as prop
 import tokens
 
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+bot = Bot(token=tokens.TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def process_start_command(msg: types.Message):
+    await bot.send_message(msg.from_user.id, prop.START_MSG)
+
+@dp.message_handler(commands=["help"])
+async def process_help_command(msg: types.Message):
+    await bot.send_message(msg.from_user.id, prop.HELP_MSG)
+
+
+@dp.message_handler(commands=['photo'])
+async def process_photo_command(msg: types.Message):
+    photo = types.input_file.InputFile("images/output.jpg")
+    await bot.send_photo(msg.from_user.id, photo)
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
+'''
 content_img = Image.open("images/content_image.jpg")
 style_img = Image.open("images/style_image.jpg")
 
@@ -17,3 +48,4 @@ style_transformer = st_tr.StyleTransformer()
 tensor_image = style_transformer(content_img, style_img, num_steps=prop.NUM_STEPS).squeeze(0)
 image = transforms.ToPILImage(mode='RGB')(tensor_image)
 image.save("images/output.jpg")
+'''
